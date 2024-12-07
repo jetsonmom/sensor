56ac35afe0404bd4326eb85099b83357d9402e09
jetsonmom <92077615+jetsonmom@users.noreply.github.com>
2024-12-08 02:29:07 +0900

    Update co2_arduino_only_serial.ino

--- co2_arduino_only_serial.ino
+++ co2_arduino_only_serial.ino
@@ -5,18 +5,18 @@ unsigned char Send_data[4] = {0x11,0x01,0x01,0xED};
 unsigned char Receive_Buff[8];
 unsigned char recv_cnt = 0;
 unsigned int PPM_Value;
- 
+
 void Send_CMD(void) {
-  unsigned int i;
-  for(i=0; i<4; i++) {
+  for(int i=0; i<4; i++) {
     mySerial.write(Send_data[i]);
     delay(1);
   }
 }
+
 unsigned char Checksum_cal(void) {
-  unsigned char count, SUM=0;
-  for(count=0; count<7; count++) {
-     SUM += Receive_Buff[count];
+  unsigned char SUM=0;
+  for(unsigned char count=0; count<7; count++) {
+    SUM += Receive_Buff[count];
   }
   return 256-SUM;
 }
@@ -25,28 +25,39 @@ void setup() {
   pinMode(13,INPUT);
   pinMode(11,OUTPUT);
   Serial.begin(9600);
-  while (!Serial) ;
+  Serial.println("Starting CO2 measurements...");
+  while (!Serial);
   mySerial.begin(9600);
   while (!mySerial);
 }
  
 void loop() {
-  Serial.print("Sending...");
   Send_CMD();
+ 
   while(1) {
     if(mySerial.available()) { 
-       Receive_Buff[recv_cnt++] = mySerial.read();
-      if(recv_cnt ==8){recv_cnt = 0; break;}
+      Receive_Buff[recv_cnt++] = mySerial.read();
+      if(recv_cnt == 8) {
+        recv_cnt = 0;
+        break;
+      }
     }
   } 
   
   if(Checksum_cal() == Receive_Buff[7]) {
-     PPM_Value = Receive_Buff[3]<<8 | Receive_Buff[4];
-     Serial.write("   PPM : ");
-     Serial.println(PPM_Value);
+    PPM_Value = Receive_Buff[3]<<8 | Receive_Buff[4];
+    
+    // CO2 보정
+    float calibration_factor = 0.2;  // 보정 계수
+    float base_co2 = 400.0;          // 기본 CO2 농도
+    PPM_Value = (PPM_Value * calibration_factor) + base_co2;
+    
+    Serial.print("CO2: ");
+    Serial.print(PPM_Value);
+    Serial.println(" ppm");
+  } else {
+    Serial.println("Checksum Error");
   }
-   else {
-    Serial.write("CHECKSUM Error");
-  }
-  delay(1000);
-} 
+  
+  delay(1000);  // 1초 딜레이
+}
